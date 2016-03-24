2be5020f90b369709de81894b24034fef778837b
Christian Hent <hent.dev@googlemail.com>
2016-03-24 13:56:15 +0100

    added compatibility with joomla 3.5

--- sql/install.mysql.utf8.sql
+++ sql/install.mysql.utf8.sql
@@ -3,12 +3,12 @@ CREATE TABLE IF NOT EXISTS `#__zatracks` (
   `context` varchar(50) NOT NULL,
   `name` varchar(255) NOT NULL,
   `activity` tinyint(3) NOT NULL DEFAULT '1',
-  `geojson` longtext CHARACTER SET utf8 NOT NULL,
-  `polyline` longtext CHARACTER SET utf8 NOT NULL,
+  `geojson` mediumtext CHARACTER SET utf8 NOT NULL,
+  `polyline` mediumtext CHARACTER SET utf8 NOT NULL,
   `distance` decimal(5,2) NOT NULL,
   `duration` bigint(20) NOT NULL,
   `starttime` datetime NOT NULL,
   `avs` decimal(4,2) NOT NULL,
   `strava_activity_id` bigint(20) NOT NULL,
-  `custom` varchar(255) NOT NULL
+  `custom` mediumtext CHARACTER SET utf8 NOT NULL
 );
